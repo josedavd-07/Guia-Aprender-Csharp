1ca167e59573aea484fefd55e31d251dd7eadf11
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-08-08 22:07:26 -0500

    Añadimos la seccion  de COmentarios y actualizamamso el temario del readme

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 00. Aprende C#
 01. Introduccion Hello World
 02. Salida de datos
-03. sintaxis de C#
+03. Sintaxis de C#
+04. Comentarios in C#
---
+++ _04_comentarios/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//Esto es un cokentario de una sola linea de manera maás corta.
+/*
+Esto es un comentario de varias lineas
+de manera mas corta.
+*/
+//Esto es un comentario de una sola linea de manera mas larga.
+/*
+Esto es un comentario de varias lineas
+de manera mas larga.
+*/
+
+
